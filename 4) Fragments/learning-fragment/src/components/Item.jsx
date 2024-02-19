8a520d2280b8c94fbf0add2eb1ff21506b584f54
im-gitking <avipal577@gmail.com>
2024-02-19 18:07:25 +0530

    CSS Module

--- 4) Fragments/learning-fragment/src/components/Item.jsx
+++ 4) Fragments/learning-fragment/src/components/Item.jsx
@@ -1,10 +1,15 @@
+import styles from "./Item.module.css";
+
 const Item = ({ foodItem }) => {
   // Object Destructuing
   // let { foodItem } = props;
 
+  // console.log(styles);
+
   return (
-    <li key={foodItem} className="list-group-item">
-      {foodItem}
+    <li key={foodItem} className={`list-group-item ${styles["kg-item"]}`}>
+      {/* <li key={foodItem} className="list-group-item kg-item"> */}
+      <span className={styles["kg-item"]}>{foodItem}</span>
     </li>
   );
 };
