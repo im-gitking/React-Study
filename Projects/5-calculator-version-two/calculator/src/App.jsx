474db2cdfcf100653b254d298e53a46066e70573
im-gitking <avipal577@gmail.com>
2024-02-21 23:12:02 +0530

    Project: Calculator v2

---
+++ Projects/5-calculator-version-two/calculator/src/App.jsx
@@ -0,0 +1,30 @@
+import styles from "./App.module.css";
+import Display from "./components/Display";
+import ButtonsContainer from "./components/ButtonsContainer";
+import { useState } from "react";
+
+function App() {
+  let [calVal, setCalVal] = useState("45");
+
+  const onButtonClick = (buttonText) => {
+    console.log(buttonText);
+    if (buttonText === "C") {
+      setCalVal("");
+    } else if (buttonText === "=") {
+      const result = eval(calVal);
+      setCalVal(result);
+    } else {
+      let newDisplayValue = calVal + buttonText;
+      setCalVal(newDisplayValue);
+    }
+  };
+
+  return (
+    <div className={styles.calculator}>
+      <Display displayValue={calVal}></Display>
+      <ButtonsContainer onButtonClick={onButtonClick}></ButtonsContainer>
+    </div>
+  );
+}
+
+export default App;
